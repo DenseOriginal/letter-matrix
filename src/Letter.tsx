e2d9237f2b060e88650c4f11de2bb3439b237302
Anders Kok <ak.kornerup@gmail.com>
2023-11-07 22:38:44 +0100

    Convert to svg elements

--- src/Letter.tsx
+++ src/Letter.tsx
@@ -1,8 +1,11 @@
 import { classNames } from "./helpers";
+import { remToPx } from "./Out";
 import { Letter } from "./types"
 
 interface Props {
 	letter: Letter;
+	row: number;
+	col: number;
 	selected?: string;
 	keyMode?: boolean;
 }
@@ -11,11 +14,32 @@ export const LetterEl = (props: Props) => {
 	const highlight = props.letter.parents.includes(props.selected || '');
 	const keyMode = props.keyMode;
 
+	const rem = remToPx(1);
+
+	return keyMode ? (
+		highlight ? (
+			<rect x={props.col * rem} y={props.row * rem} width={rem} height={rem} fill="white" />
+		) : null
+	) : <>
+		{/* <rect x={props.col * rem} y={props.row * rem} width={rem} height={rem} stroke="white" strokeWidth={1} /> */}
+		{highlight && <rect x={props.col * rem} y={props.row * rem} width={rem} height={rem} fill="red" />}
+		<text
+			x={props.col * rem + 3}
+			y={props.row * rem + rem - 2.5}
+			width={rem}
+			height={rem}
+			fontFamily="Roboto mono"
+			fill="white"
+			fontSize={rem}
+			style={{ userSelect: 'none' }}
+		>{props.letter.char}</text>
+	</>
+
 	return <span
 		className={classNames(
-			"leading-[16px] w-4 grid place-content-center text-white",
+			"leading-[1rem] w-4 grid place-content-center text-white",
 			!keyMode && highlight && 'bg-red-600',
-			keyMode && !highlight && 'text-black',
+			keyMode && !highlight && '!text-black',
 			keyMode && highlight && 'bg-white text-white'
 		)}
 	>{props.letter.char}</span>
--- src/Out.tsx
+++ src/Out.tsx
@@ -11,19 +11,63 @@ interface Props {
 }
 
 export function Out(props: Props) {
-	const rows = split(props.code, props.columns);
-
-	return <div className='p-2 bg-black rounded'>
-		{props.name && <h2 className='text-xl text-white uppercase mb-2'>#{props.name}</h2>}
-		<div>
-			{rows.map((row, idx) => <div className='flex' key={idx}>
-				{row.map((letter, idx) => <LetterEl
-					key={letter.char + idx}
-					letter={letter}
-					selected={props.highlight || props.name}
-					keyMode={props.keyMode}
-				/>)}
-			</div>)}
-		</div>
-	</div>
+	return <figure>
+		<SvgElement {...props} />
+	</figure>
 }
+
+
+function SvgElement(props: Props) {
+	const rows = split(props.code, props.columns);
+	const width = getWidthInPx(props.columns);
+	const height = getHeightInPx(rows.length, !!props.name);
+
+	const paddingPx = remToPx(padding);
+
+	const codeViewOffsetX = paddingPx;
+	const codeViewOffsetY = paddingPx + (props.name ? remToPx(nameHeight) : 0);
+	const codeViewHeight = remToPx(rows.length);
+	const codeViewWidth = remToPx(props.columns);
+
+	return <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox={`0 0 ${width} ${height}`}>
+		<defs>
+			<style>
+				@import url("https://fonts.googleapis.com/css?family=Roboto:400,400i,700,700i");
+			</style>
+		</defs>
+		<rect x="0" y="0" width="100%" height="100%" rx="6" fill="#202029" />
+		{props.name && <text
+			x={paddingPx}
+			y={paddingPx + remToPx(nameFontSize) - 4}
+			fontFamily="Roboto Mono"
+			textAnchor="start"
+			fill="white"
+			fontSize={`${nameFontSize}rem`}
+			style={{ textTransform: 'uppercase' }}
+		>#{props.name}</text>}
+		<svg
+			viewBox={`0 0 ${codeViewWidth} ${codeViewHeight}`}
+			width={codeViewWidth}
+			height={codeViewHeight}
+			x={codeViewOffsetX}
+			y={codeViewOffsetY}
+		>
+			{rows.map((row, rowIdx) => row.map((letter, colIdx) => <LetterEl
+				key={letter.char + rowIdx + colIdx}
+				row={rowIdx}
+				col={colIdx}
+				letter={letter}
+				selected={props.highlight || props.name}
+				keyMode={props.keyMode}
+			/>))}
+		</svg>
+	</svg>
+}
+
+const nameFontSize = 1.75;
+const padding = 0.5;
+const nameHeight = nameFontSize + 0.5;
+const getWidthInPx = (columns: number) => remToPx((padding * 2) + columns);
+const getHeightInPx = (rows: number, withName = false) => remToPx((padding * 2) + rows + (withName ? nameHeight : 0));
+
+export const remToPx = (rem: number) => rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
